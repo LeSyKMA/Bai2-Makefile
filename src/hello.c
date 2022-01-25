1a6b92ec809b41c4950a756ba4f51c2aacd6225b
= <=>
2022-01-25 23:52:00 +0700

    Test Makefile

---
+++ src/hello.c
@@ -0,0 +1,4 @@
+#include"..\include\hello.h"
+void show_name(void *name){
+    printf("Hello: %s",(char*)name);
+}
---
+++ src/main.c
@@ -0,0 +1,5 @@
+#include"../include/hello.h"
+int main(int argc,char*argv[]){
+    show_name("le van sy DT2B");
+    return 0;
+}
